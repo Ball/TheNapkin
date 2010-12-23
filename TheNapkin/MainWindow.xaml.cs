dd5bb923ec0e72f428585cc72ab81ee8763705e1
Brian J Ball <myotherpants@gmail.com>
2010-12-23 17:02:09 -0500

    Refactor after the bug-2

--- TheNapkin/MainWindow.xaml.cs
+++ TheNapkin/MainWindow.xaml.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TheNapkin
@@ -17,11 +10,47 @@ namespace TheNapkin
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private readonly IDisposable _mouseSub;
+
         public MainWindow()
         {
             InitializeComponent();
+            Unloaded += This_Unloaded;
+
+            var stylusDown = Observable.FromEvent<StylusDownEventArgs>(theCanvas, "StylusDown");
+            var stylusUp   = Observable.FromEvent<StylusEventArgs>(theCanvas, "StylusUp");
+            var stylusMove = Observable.FromEvent<StylusEventArgs>(theCanvas, "StylusMove")
+                .Select(mm => mm.EventArgs.GetPosition(theCanvas))
+                .Select(p  => new {p.X, p.Y});
+
+            var stylusDiffs = stylusMove
+                .Skip(1)
+                .Zip(stylusMove, (l, r) => new { X1 = l.X, Y1 = l.Y, X2 = r.X, Y2 = r.Y });
+
+            var stylusDrag = from __ in stylusDown
+                             from md in stylusDiffs.TakeUntil(stylusUp)
+                             select md;
+            _mouseSub = stylusDrag.Subscribe(item =>
+            {
+                var line = new Line
+                {
+                    Stroke = Brushes.Black,
+                    X1 = item.X1,
+                    X2 = item.X2,
+                    Y1 = item.Y1,
+                    Y2 = item.Y2,
+                    StrokeThickness = 1
+                };
+                theCanvas.Children.Add(line);
+            });
+        }
+
+        private void This_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Unloaded -= This_Unloaded;
+            _mouseSub.Dispose();
         }
     }
 }
